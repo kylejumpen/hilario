fa21de806173c096b4e2ec39af2880e07eded453
kyle <kylecs.ka@gmail.com>
2016-06-27 08:22:22 -0400

    homepage

--- Code/Java/src/fr/kaf/app/Main.java
+++ Code/Java/src/fr/kaf/app/Main.java
@@ -1,19 +1,18 @@
 package fr.kaf.app;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-
+import javafx.scene.*;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("fr.kaf.app.css").toExternalForm());
-			primaryStage.setScene(scene);
+			Parent root = FXMLLoader.load(getClass().getResource("/fr/kaf/app/fxml/Home.fxml"));
+			primaryStage.setScene(new Scene(root));
+			primaryStage.setTitle("Hilario");
+			primaryStage.setResizable(false);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
